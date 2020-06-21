805d9da9446085b63933f0c0c41b7618733b6f35
Lewis Morgans <lmorgans90@gmail.com>
2020-06-21 16:19:16 +0100

    complete minus testing

--- src/app/pages/index-component/index-component.ts
+++ src/app/pages/index-component/index-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http-service/http.service';
 import { Observable, interval } from 'rxjs';
 import { switchMap, startWith } from 'rxjs/operators'
@@ -6,20 +6,36 @@ import { switchMap, startWith } from 'rxjs/operators'
 @Component({
   selector: 'index-component',
   templateUrl: './index-component.html',
-  styleUrls: ['./index-component.css']
+  styleUrls: ['./index-component.scss']
 })
 
-export class IndexComponent {
+export class IndexComponent implements OnInit {
 
-  public iconUrl = "https://image.flaticon.com/icons/svg/547/547424.svg";
-  public initSateliteLocation$: Observable<ISSResponse> = this._httpService.getLocation$();
+  public iconUrl: Object;
+  public zoom = 2;
   public sateliteData$: Observable<ISSResponse> = interval(5000)
-  .pipe(
-    startWith(this._httpService.getLocation$()),
-    switchMap(_ => this._httpService.getLocation$()),
-  )
+    .pipe(
+      startWith(this._httpService.getLocation$()),
+      switchMap(_ => this._httpService.getLocation$()),
+    )
 
   constructor(private readonly _httpService: HttpService) { }
 
+  ngOnInit() {
+    this.initializeState();
+  }
 
+  public updateZoomInput(event: number) {
+    this.zoom = event;
+  }
+
+  private initializeState(): void {
+    this.iconUrl = {
+      url: "https://image.flaticon.com/icons/svg/547/547424.svg",
+      scaledSize: {
+        height: 40,
+        width: 40
+      }
+    }
+  }
 }
